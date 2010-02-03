1d86bbdf99f794586d658d610707c99a014ed727
Michele Franzin <michele.franzin@gmail.com>
2010-02-03 23:40:44 +0100

    initial commit

--- lib/jrails_auto_complete.rb
+++ lib/jrails_auto_complete.rb
@@ -7,13 +7,13 @@ module JrailsAutoComplete
     def auto_complete_for(object_name, method_name, options = {})
       self.send(:define_method, "auto_complete_for_#{object_name}_#{method_name}") do
         find_options = {
-          :conditions => [ "LOWER(#{method_name}) LIKE ?", '%' + params[object_name][method_name].downcase + '%' ],
+          :conditions => [ "LOWER(#{method_name}) LIKE ?", '%' + params['term'].downcase + '%' ],
           :order => "#{method_name} ASC",
           :limit => 10
         }.merge!(options)
 
         @items = object_name.to_s.camelize.constantize.find(:all, find_options)
-        render :inline => "<%= auto_complete_result @items, '#{method_name}' %>"
+        render :inline => "<%= auto_complete_result @items, '#{method_name}', '#{params['term']}' %>"
       end
     end
   end
