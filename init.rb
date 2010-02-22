cf08c30f498f71541bf5a72f47ad82d02d68ce32
Michele Franzin <michele.franzin@gmail.com>
2010-02-22 03:21:23 +0100

    extracted form buider + istance tag helpers from main module

--- init.rb
+++ init.rb
@@ -1,2 +1,4 @@
 ActionController::Base.send :include, JrailsAutoComplete
 ActionController::Base.helper JrailsAutoCompleteMacroHelper
+ActionView::Helpers::FormBuilder.send :include, JrailsAutoCompleteFormBuilderHelper
+ActionView::Helpers::TagHelper.send :include, JrailsAutoCompleteTagHelper
