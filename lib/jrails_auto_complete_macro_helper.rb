78c2fd64abbc21909b22ebcf14891e87709fee2c
Michele Franzin <michele.franzin@gmail.com>
2010-02-24 16:06:17 +0100

    drop duplicated entries before json convertion

--- lib/jrails_auto_complete_macro_helper.rb
+++ lib/jrails_auto_complete_macro_helper.rb
@@ -2,20 +2,20 @@ module JrailsAutoCompleteMacroHelper
   def auto_complete_field(field_id, options = {})
     js_options = {}
 
-#    if %w(put delete).include?(options[:method])
-#      options[:defaultParams] = (options[:defaultParams].blank?? '' : options[:defaultParams] + '&') +
-#        "_method=#{options[:method].to_s}"
-#      options[:method] = :post
-#    end
-#    if options[:method] == :post and protect_against_forgery?
-#      options[:defaultParams] = (options[:defaultParams].blank?? '' : options[:defaultParams] + '&') +
-#        "#{h(request_forgery_protection_token.to_s)}=#{h(form_authenticity_token)}"
-#    end
+    #    if %w(put delete).include?(options[:method])
+    #      options[:defaultParams] = (options[:defaultParams].blank?? '' : options[:defaultParams] + '&') +
+    #        "_method=#{options[:method].to_s}"
+    #      options[:method] = :post
+    #    end
+    #    if options[:method] == :post and protect_against_forgery?
+    #      options[:defaultParams] = (options[:defaultParams].blank?? '' : options[:defaultParams] + '&') +
+    #        "#{h(request_forgery_protection_token.to_s)}=#{h(form_authenticity_token)}"
+    #    end
 
     js_options[:source]        = "'#{url_for(options[:source])}'"
-#    js_options[:type]          = "'#{options[:method].to_s.upcase}'" if options[:method]
+    #    js_options[:type]          = "'#{options[:method].to_s.upcase}'" if options[:method]
     js_options[:update]        = "'" + (options[:update] || "#{field_id}_auto_complete") + "'"
-#    js_options[:defaultParams] = "'#{options[:defaultParams]}'" if options[:defaultParams]
+    #    js_options[:defaultParams] = "'#{options[:defaultParams]}'" if options[:defaultParams]
 
     {:min_length => :minLength, :delay => :delay}.each do |k, v|
       js_options[v] = options[k] if options[k]
@@ -33,11 +33,12 @@ module JrailsAutoCompleteMacroHelper
 
   def auto_complete_result(entries, field, phrase = nil)
     return unless entries
-    # items = entries.map { |entry| content_tag('li', phrase ? highlight(entry.send(field), phrase) : h(entry[field])) }
-    # content_tag('ul', items.uniq)
-    #entries.map! { |entry| entry[field] }
-    entries.map! { |entry| { 'id' => entry[:id], 'value' => h(entry[field]), 'label' => phrase ? highlight(entry.send(field), phrase) : h(entry[field]) } }
-    entries.to_json
+    unique_entries= entries.uniq.map do |entry|
+      {'id' => entry[:id],
+        'value' => h(entry[field]),
+        'label' => phrase ? highlight(entry.send(field), phrase) : h(entry[field])}
+    end
+    unique_entries.to_json
   end
 
   def auto_complete_for(object, method, options = {})
