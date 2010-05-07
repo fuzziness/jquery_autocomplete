4508d13c7bc6a764ec370faf9dd7f7202b4f4895
Michele Franzin <michele.franzin@gmail.com>
2010-05-07 10:49:57 +0200

    updated to jquery-ui ver 1.8.1

--- uninstall.rb
+++ uninstall.rb
@@ -1,6 +1,6 @@
 puts "Removing files..."
 dir = "javascripts"
-["jquery-ui.js jquery-ui-i18n.min.js"].each do |js_file|
+["jquery-ui.min.js jquery-ui-i18n.min.js"].each do |js_file|
 	dest_file = File.join(Rails.root, "public", dir, js_file)
 	FileUtils.rm_r(dest_file)
 end
