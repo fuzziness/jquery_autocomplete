20a8464823c3df4b29b00dc7caf9f1e3c24cbb7a
Michele Franzin <michele.franzin@gmail.com>
2010-05-07 17:32:38 +0200

    added named scope items filtering

--- lib/jrails_auto_complete.rb
+++ lib/jrails_auto_complete.rb
@@ -26,15 +26,17 @@ module JrailsAutoComplete
   module ClassMethods
     def auto_complete_for(object_name, method_name, options = {})
       self.send(:define_method, "auto_complete_for_#{object_name}_#{method_name}") do
-        find_options = {
-          :conditions => [ "LOWER(#{method_name}) LIKE ?", '%' + params['term'].downcase + '%' ],
-          :order => "#{method_name} ASC",
+        model = object_name.to_s.camelize.constantize
+        full_method_name = "#{model.quoted_table_name}.#{method_name}"
+        find_options= {
+          :conditions => ["LOWER(#{full_method_name}) LIKE ?", '%' + params['term'].downcase + '%'],
+          :order => "#{full_method_name} ASC",
           :limit => 10
         }.merge!(options)
 
-        @items = object_name.to_s.camelize.constantize.find(:all, find_options)
-
-        @items = yield(@items) if block_given?
+        @items = model.scoped(find_options)
+        @items = yield(@items, params) if block_given?
+        
         render :inline => "<%= auto_complete_result @items, '#{method_name}', '#{params['term']}' %>"
       end
     end
