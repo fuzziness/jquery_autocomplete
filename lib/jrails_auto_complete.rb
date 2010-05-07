a61fc0f893e2a6db25bb5ef493583feb46e6cddc
Michele Franzin <michele.franzin@gmail.com>
2010-05-07 12:55:21 +0200

    CHANGELOG, VERSION, minimal rdocs, unused code cleanup

--- lib/jrails_auto_complete.rb
+++ lib/jrails_auto_complete.rb
@@ -3,6 +3,26 @@ module JrailsAutoComplete
     base.extend(ClassMethods)
   end
 
+  #
+  # Example:
+  #
+  #   # Controller
+  #   class BlogController < ApplicationController
+  #     auto_complete_for :post, :title
+  #   end
+  #
+  #   # View
+  #   <%= text_field_with_auto_complete :post, title %>
+  #
+  # By default, auto_complete_for limits the results to 10 entries,
+  # and sorts by the given field.
+  #
+  # auto_complete_for takes a third parameter, an options hash to
+  # the find method used to search for the records:
+  #
+  #   auto_complete_for :post, :title, :limit => 15, :order => 'created_at DESC'
+  #    def auto_complete_for(object_name, method_name, options = {})
+  #
   module ClassMethods
     def auto_complete_for(object_name, method_name, options = {})
       self.send(:define_method, "auto_complete_for_#{object_name}_#{method_name}") do
--- lib/jrails_auto_complete_macros_helper.rb
+++ lib/jrails_auto_complete_macros_helper.rb
@@ -1,19 +1,48 @@
 module JrailsAutoCompleteMacrosHelper
+  # Adds JQuery autocomplete functionality to the text input field with the
+  # DOM ID specified by +field_id+.
+  #
+  # You'll probably want to turn the browser's built-in autocompletion off,
+  # so be sure to include an <tt>autocomplete="off"</tt> attribute with your text
+  # input field.
+  #
+  # Required +options+ are:
+  # <tt>:source</tt>::               URL to call for autocompletion results
+  #                                  in url_for format.
+  #
+  # Addtional +options+ are:
+  # <tt>:update</tt>::               Specifies the DOM ID of the element whose
+  #                                  innerHTML should be updated with the autocomplete
+  #                                  entries returned by the request.
+  #                                  Defaults to <tt>field_id</tt> + '_auto_complete'
+  # <tt>:delay</tt>::                The delay in milliseconds the Autocomplete waits after
+  #                                  a keystroke to activate itself. A zero-delay makes sense
+  #                                  for local data (more responsive), but can produce a lot
+  #                                  of load for remote data, while being less responsive.
+  # <tt>:min_length</tt>::           The minimum number of characters a user has to type
+  #                                  before the Autocomplete activates. Zero is useful for
+  #                                  local data with just a few items. Should be increased
+  #                                  when there are a lot of items, where a single character
+  #                                  would match a few thousand items.
+  # <tt>:open</tt>::                 Before a request (source-option) is started, after
+  #                                  minLength and delay are met. Can be canceled (return false),
+  #                                  then no request will be started and no items suggested.
+  # <tt>:focus</tt>::                Before focus is moved to an item (not selecting), ui.item
+  #                                  refers to the focused item. The default action of focus is
+  #                                  to replace the text field's value with the value of the focused
+  #                                  item. Canceling this event prevents the value from being updated,
+  #                                  but does not prevent the menu item from being focused.
+  # <tt>:select</tt>::               Triggered when an item is selected from the menu; ui.item
+  #                                  refers to the selected item. The default action of select is
+  #                                  to replace the text field's value with the value of the
+  #                                  selected item. Canceling this event prevents the value from
+  #                                  being updated, but does not prevent the menu from closing.
+  # <tt>:close</tt>::                When the list is hidden - doesn't have to occur together
+  #                                  with a change.
+  # <tt>:change</tt>::               After an item was selected; ui.item refers to the selected
+  #                                  item. Always triggered after the close event.
   def auto_complete_field(field_id, options = {})
     js_options = {}
-
-    #    if %w(put delete).include?(options[:method])
-    #      options[:defaultParams] = (options[:defaultParams].blank?? '' : options[:defaultParams] + '&') +
-    #        "_method=#{options[:method].to_s}"
-    #      options[:method] = :post
-    #    end
-    #    if options[:method] == :post and protect_against_forgery?
-    #      options[:defaultParams] = (options[:defaultParams].blank?? '' : options[:defaultParams] + '&') +
-    #        "#{h(request_forgery_protection_token.to_s)}=#{h(form_authenticity_token)}"
-    #    end
-    #    js_options[:type]          = "'#{options[:method].to_s.upcase}'" if options[:method]
-    #    js_options[:defaultParams] = "'#{options[:defaultParams]}'" if options[:defaultParams]
-
     js_options[:source]        = "'#{url_for(options[:source])}'"
     js_options[:update]        = "'" + (options[:update] || "#{field_id}_auto_complete") + "'"
 
@@ -22,7 +51,7 @@ module JrailsAutoCompleteMacrosHelper
     end
 
     [:search, :open, :focus, :select, :close, :change].each do |k|
-      js_options[k] = "function(event, ui) { #{options[k]} }" if options[k]
+      js_options[k] = "function(event,ui){#{options[k]}}" if options[k]
     end
 
     function = "#{ActionView::Helpers::PrototypeHelper::JQUERY_VAR}('##{field_id}').autocomplete("
